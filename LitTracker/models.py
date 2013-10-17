f7fe24884d0eed974d5832d3393b8a5636112dbc
allen.oster <allen.oster@rackspace.com>
2013-10-16 20:08:35 -0500

    Models in place and admin working.

--- LitTracker/models.py
+++ LitTracker/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Item(models.Model):
+    item = models.CharField(max_length=200)
+    code = models.CharField(max_length=20, null=True, blank=True)
+
+    def __unicode__(self):
+        return unicode(self.item)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+
+class Order(models.Model):
+    item = models.ForeignKey(Item)
+    ordered_by = models.ForeignKey(Publisher)
+    order_date = models.DateTimeField('Date Ordered')
+    received_date = models.DateTimeField('Date Received', null=True)
+
+    def __unicode__(self):
+        return unicode(self.item)
+
+class InStock(models.Model):
+    item = models.ForeignKey(Item)
+    publisher = models.ForeignKey(Publisher)
+
+    def __unicode__(self):
+        return unicode(self.item)
